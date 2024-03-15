50a4e2159b37420286c3f5a9bbb26718955fd4fe
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-03-15 17:16:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let student={
+	name:"saurabh",
+	age:23
+}
+Object.prototype.getKeys=function(){
+	let result=[];
+	for(let key in this){
+		if(this.hasOwnProperty(key)){
+			result.push(key)
+		}
+		
+	}
+	return result;
+}
